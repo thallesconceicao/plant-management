eefc40490e8b2e18673dfd6c95a3dca3aeae4c19
Thalles Conceição <thallesangelo@gmail.com>
2021-04-20 22:37:53 -0300

    First commit and Welcome Page

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text, SafeAreaView, StyleSheet } from 'react-native'
+import { Welcome } from './src/pages/Welcome'
+
+export default function App(){
+  return (
+    <Welcome />
+  )
+}
