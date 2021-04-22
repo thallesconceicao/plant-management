fb7eb0f61074c2226e983a8bd5dfc99aa5891b0b
Thalles Conceição <thallesangelo@gmail.com>
2021-04-22 20:05:37 -0300

    Create button component

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Text, SafeAreaView, View, StyleSheet, Image, TouchableOpacity } from 'react-native'
 
 import wateringImg from '../assets/watering.png'
+import { Button } from '../components/Button'
 import colors from '../styles/colors'
 
 export function Welcome(){
@@ -19,13 +20,7 @@ export function Welcome(){
                 Não esqueça mais de regar suas {'\n'}plantas. 
                 Nós cuidamos de lembrar você{'\n'} sempre que precisar.
             </Text>
-            <TouchableOpacity 
-                style={styles.button} 
-                activeOpacity={0.7}>
-                <Text style={styles.buttonText}>
-                    >
-                </Text>
-            </TouchableOpacity>
+            <Button title=">"></Button>
         </SafeAreaView>
     )
 }
