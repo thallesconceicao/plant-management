64d0a0eeff402bb1e52fd86b911ea808514a9878
Thalles Conceição <thallesangelo@gmail.com>
2021-04-24 11:17:33 -0300

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {
+    View,
+    StyleSheet,
+    Text,
+    Image
+} from 'react-native'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
+import colors from '../styles/colors';
+import userImg from '../assets/thalles.jpg';
+import { color } from 'react-native-reanimated';
+import fonts from '../styles/fonts';
+
+export function Header(){
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text style={styles.name}>Thalles</Text>
+            </View>
+            <Image 
+                source={userImg}
+                style={styles.image}></Image>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container : {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight()
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 40
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    name: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 40
+    }
+});
