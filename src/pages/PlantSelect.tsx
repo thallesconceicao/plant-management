446e45452f1fe2ca8ad3b7f0c6b73b26ee9d9374
Thalles Conceição <thallesangelo@gmail.com>
2021-04-24 14:57:41 -0300

    List plants and create filters for them

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -8,6 +8,7 @@ import {
 import { EnvironmentButton } from '../components/EnvironmentButton';
 
 import { Header } from '../components/Header'
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
 import api from '../services/api';
 
 import colors from '../styles/colors';
@@ -18,13 +19,42 @@ interface EnviromentProps {
     title: string;
 }
 
+interface PlantProps {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+        times: number;
+        repeat_every: string;
+      }
+}
+
 export function PlantSelect(){
 
     const [enviroments, setEnviroments] = useState<EnviromentProps[]>();
+    const [plants, setPlants] = useState<PlantProps[]>();
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>();
+    const [enviromentSelected, setEnviromentSelected] = useState('all');
+
+    function handleEnviromentSelected(enviroment: string){
+        setEnviromentSelected(enviroment);
+
+        if(enviroment == 'all')
+            return setFilteredPlants(plants);
+
+            const filtered = plants?.filter(plant =>
+                plant.environments.includes(enviroment)
+            );
+
+            setFilteredPlants(filtered);
+    }
 
     useEffect(() => {
         async function fetchEnviroment(){
-            const { data } = await api.get('plants_environments');
+            const { data } = await api.get('plants_environments?_sort=title&_order=asc');
             setEnviroments([
                 {
                     key: 'all',
@@ -35,6 +65,15 @@ export function PlantSelect(){
         }
 
         fetchEnviroment();
+    }, [])
+
+    useEffect(() => {
+        async function fetchPlants(){
+            const { data } = await api.get('plants?_sort=name&_order=asc');
+            setPlants(data)
+        }
+
+        fetchPlants();
     }, [])
 
     return (
@@ -49,11 +88,24 @@ export function PlantSelect(){
                 <FlatList 
                     data={enviroments}
                     renderItem={({ item }) => (
-                        <EnvironmentButton title={item.title}></EnvironmentButton>
+                        <EnvironmentButton 
+                            title={item.title} 
+                            active={item.key === enviromentSelected}
+                            onPress={() => handleEnviromentSelected(item.key)} />
                     )}
                     horizontal
                     showsHorizontalScrollIndicator={false}
                     contentContainerStyle={styles.enviromentList}/>
+            </View>
+
+            <View style={styles.plants}>
+                <FlatList 
+                    data={filteredPlants}
+                    renderItem={({ item }) => (
+                        <PlantCardPrimary data={item}></PlantCardPrimary>
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}/>
             </View>
         </View>
     )
@@ -86,5 +138,10 @@ const styles = StyleSheet.create({
         paddingBottom: 5,
         marginLeft: 32,
         marginVertical: 32
+    },
+    plants: {
+        flex: 1,
+        paddingHorizontal: 32,
+        justifyContent: 'center'
     }
 });
