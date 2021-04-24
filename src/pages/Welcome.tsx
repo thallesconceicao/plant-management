acb75571a22066c41886edc9a606e3d50497fcff
Thalles Conceição <thallesangelo@gmail.com>
2021-04-23 23:18:47 -0300

    Adds fonts and styles to Welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,43 +1,58 @@
 import React from 'react'
-import { Text, SafeAreaView, StyleSheet, Image, TouchableOpacity, Dimensions } from 'react-native'
+import { 
+    Text, 
+    SafeAreaView, 
+    StyleSheet, 
+    Image, 
+    TouchableOpacity, 
+    Dimensions, 
+    View 
+} from 'react-native'
 
 import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 export function Welcome(){
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie{'\n'} 
-                suas plantas de{'\n'}
-                forma fácil
-            </Text>
-            <Image 
-                source={wateringImg} 
-                style={styles.image}
-                resizeMode="contain"/>
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas {'\n'}plantas. 
-                Nós cuidamos de lembrar você{'\n'} sempre que precisar.
-            </Text>
-            <TouchableOpacity 
-                style={styles.button} 
-                activeOpacity={0.7}>
-                <Feather 
-                    name="chevron-right"
-                    style={styles.buttonIcon}/>
-            </TouchableOpacity>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie{'\n'} 
+                    suas plantas de{'\n'}
+                    forma fácil
+                </Text>
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image}
+                    resizeMode="contain"/>
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas {'\n'}plantas. 
+                    Nós cuidamos de lembrar você{'\n'} sempre que precisar.
+                </Text>
+                <TouchableOpacity 
+                    style={styles.button} 
+                    activeOpacity={0.7}>
+                    <Feather 
+                        name="chevron-right"
+                        style={styles.buttonIcon}/>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    wrapper:{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
         fontSize: 32,
@@ -45,13 +60,15 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: colors.heading,
         marginTop: 38,
-        lineHeight: 38
+        lineHeight: 38,
+        fontFamily: fonts.heading
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
          paddingHorizontal: 20,
-         color: colors.heading
+         color: colors.heading,
+         fontFamily: fonts.text
     },
     image: {
         height: Dimensions.get('window').width * 0.7
