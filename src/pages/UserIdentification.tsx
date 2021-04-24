b47250031e1f2617918dee100dd8cd7cc6f36baa
Thalles Conceição <thallesangelo@gmail.com>
2021-04-24 00:48:58 -0300

    Closes keyboard after user clicks outside the input

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -7,7 +7,9 @@ import {
     Text,
     TextInput,
     KeyboardAvoidingView,
-    Platform
+    Platform,
+    TouchableWithoutFeedback,
+    Keyboard
 } from 'react-native'
 
 import { Button } from '../components/Button'
@@ -47,34 +49,36 @@ export function UserIdentification(){
             <KeyboardAvoidingView
                 style={styles.container}
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-                <View style={styles.content}>
-                    <View style={styles.form}>
-                        <View style={styles.header}>
-                            <Text style={styles.emoji}>
-                                { isFilled ? '😄' : '😃'}
-                            </Text> 
-                            <Text style={styles.title}>
-                                Como podemos{'\n'}
-                                chamar você?
-                            </Text> 
-                        </View>
-                        <TextInput 
-                            style={[
-                                styles.input,
-                                (isFocused || isFilled) && 
-                                { borderColor: colors.green}
-                            ]} 
-                            placeholder="Digite um nome"
-                            onBlur={handleInputBlur}
-                            onFocus={handleInputFocus}
-                            onChangeText={handleInputChange}/>
-                        <View style={styles.footer}>
-                            <Button 
-                                title="Confirmar"
-                                onPress={handleConfirmation}></Button>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <View style={styles.header}>
+                                <Text style={styles.emoji}>
+                                    { isFilled ? '😄' : '😃'}
+                                </Text> 
+                                <Text style={styles.title}>
+                                    Como podemos{'\n'}
+                                    chamar você?
+                                </Text> 
+                            </View>
+                            <TextInput 
+                                style={[
+                                    styles.input,
+                                    (isFocused || isFilled) && 
+                                    { borderColor: colors.green}
+                                ]} 
+                                placeholder="Digite um nome"
+                                onBlur={handleInputBlur}
+                                onFocus={handleInputFocus}
+                                onChangeText={handleInputChange}/>
+                            <View style={styles.footer}>
+                                <Button 
+                                    title="Confirmar"
+                                    onPress={handleConfirmation}></Button>
+                            </View>
                         </View>
                     </View>
-                </View>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
