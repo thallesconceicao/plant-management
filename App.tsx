0dd35bc0830992f789e64ec64f1ebc3a6c5a5ab9
Thalles Conceição <thallesangelo@gmail.com>
2021-04-24 00:11:55 -0300

    Create Confirmation Screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { UserIdentification } from './src/pages/UserIdentification'
+import { Confirmation } from './src/pages/Confirmation'
 import AppLoading from 'expo-app-loading'
 import { 
   useFonts,
@@ -18,6 +18,6 @@ export default function App(){
     return <AppLoading />
 
   return (
-    <UserIdentification />
+    <Confirmation />
   )
 }
