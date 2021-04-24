018c2fb75b5f74cf5b6a64d6291cab22074b2df1
Thalles Conceição <thallesangelo@gmail.com>
2021-04-24 11:18:17 -0300

    Update PlantSelect page

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+
+import { Header } from '../components/Header'
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSelect(){
     return (
         <View style={styles.container}>
-            <Text>
-                Selecionar Planta
-            </Text>
+            <View style={styles.header}>
+                <Header />
+                <Text style={styles.title}>Em qual ambiente</Text>
+                <Text style={styles.subtitle}>você quer colocar a sua planta?</Text>
+            </View>
         </View>
     )
 }
@@ -15,8 +21,22 @@ export function PlantSelect(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: colors.background,
-        justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: colors.background
+    },
+    header: {
+        paddingHorizontal: 30
+    },
+    title: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+    subtitle: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.text,
+        lineHeight: 20
     }
 });
